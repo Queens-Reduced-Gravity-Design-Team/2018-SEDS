ff4bae9693e5cc660b78564118e61188d91afa15
Viraj Bangari <Viraj3f@gmail.com>
2018-05-14 22:46:21 -0700

    Add comments.

--- controller/main/main.ino
+++ controller/main/main.ino
@@ -37,25 +37,26 @@ void listenToFrontend()
   // an issue if more or less than 4-bytes are ever transmitted.
   if (Serial.available() > 0)
   {
-    State* newState;
-    char stateBuffer[4];
-    stateBuffer[0] = Serial.read();
+    // Assume incoming data is kl mn op qr, with each leter represnting
+    // a half-byte (4 bits).
+    int newState = 0x00000000;
+    
+    newState |= ((unsigned char)Serial.read()) >> 0;  // LHS: kl 00 00 00 RHS: kl 00 00 00
 
     // After the first byte is read, block until all remaining bytes are
     // read.
-    while (!Serial.read());
-    stateBuffer[1] = Serial.read();
+    while (!Serial.available());
+    newState |= ((unsigned char)Serial.read()) >> 4;  // LHS: kl mn 00 00 RHS: 00 mn 00 00
 
-    while (!Serial.read());
-    stateBuffer[2] = Serial.read();
+    while (!Serial.available());
+    newState |= ((unsigned char)Serial.read()) >> 8;  // LHS: kl mn op 00 RHS: 00 00 op 00
 
-    while (!Serial.read());
-    stateBuffer[3] = Serial.read();
-    newState = (State*)(&stateBuffer);
+    while (!Serial.available());
+    newState |= ((unsigned char)Serial.read()) >> 12; // LHS: kl mn op qr RHS: 00 00 00 qr
     
-    if (FIRST_STATE <= *newState && *newState <= LAST_STATE)
+    if (FIRST_STATE <= (State)newState && (State)newState <= LAST_STATE)
     {
-      currentState = *newState;
+      currentState = (State)newState;
     }
   }
 }
@@ -64,7 +65,7 @@ void sendMessageToFrontend()
 {
   // Since Serial.write sends one byte at a time if it's not a string,
   // the current state must be sent in 4 steps.
-  Serial.write((char*)&currentState, sizeof(State));
+  Serial.write((unsigned char*)&currentState, sizeof(State));
 
   
   // NOTE: The serial protocol requires that all communications between
